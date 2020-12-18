b31ba30f5192f3025df8d2428731c62e2042def6
Adrian Mendez <adrianmndz328@gmail.com>
2020-12-18 16:37:43 -0600

    added ajax call

--- js/app.js
+++ js/app.js
@@ -1,1 +1,22 @@
-console.log("hello world");
+////////////////////////
+// PULL AND RENDER DATA
+///////////////////////
+
+$.ajax("https://spreadsheets.google.com/feeds/list/1uX9LdKDyy8BIS8HXQAoLBCbUqhaZgM5nNaFaelV8A0o/1/public/full?alt=json")
+    .then(data => {
+        console.log(data)
+
+        const rawProjects = data.feed.entry
+
+        const projects = rawProjects.map(project => {
+            return {
+                name: project.gsx$name.$t,
+                img: project.gsx$img.$t,
+                desc: project.gsx$description.$t,
+                live: project.gsx$live.$t, 
+                github: project.gsx$github.$t
+            }
+        })
+        
+        console.log(projects)
+    })
