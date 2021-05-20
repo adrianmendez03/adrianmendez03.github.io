fb05ca37f0bcdfedac637ab01113bcdf4fb69b82
Adrian Mendez <adrianmndz328@gmail.com>
2021-05-19 23:11:30 -0500

    organized the code a bit

--- js/app.js
+++ js/app.js
@@ -1,62 +1,123 @@
-////////////////////////
-// PULL AND RENDER DATA
-///////////////////////
-
-$.ajax("https://your-projects-api.herokuapp.com/projects")
-    .then(data => {
-        const projects = data.map(project => {
-            return {
-                name: project.name,
-                img: project.image,
-                desc: project.description,
-                live: project.demo, 
-                github: project.repo,
-                tech: project.tech
-            }
-        })
-        renderProj(projects)
-    })
-
-//////////////////////////////
-//// Mobile Nav Functionality
-/////////////////////////////
-
-const $logo = $("#logo")
-const $navIcon = $("#nav-icon")
-const $menu = $("#menu")
-let menuOpen = false, $scrollPos = $(window).scrollTop()
-
-const menuFunctionality = () => {
-    if (menuOpen) {
-        $menu.removeClass("open")
-        $logo.removeClass("black-text")
-        $navIcon.removeClass("open-color")
-        menuOpen = false
-    } else {
-        $menu.addClass("open")
-        $logo.addClass("black-text")
-        $navIcon.addClass("open-color")
-        menuOpen = true
-    }
-}
-
-$navIcon.click(e => {
-    menuFunctionality()
-})
-
-$("#menu").children().click(e => {
-    menuFunctionality()
-})
-
-/////////////////////////////
-/// SKILLS SCROLL
-/////////////////////////////
-
 $(() => {
+
+    /////////////////////////////////////
+    /// FETCH PROJECTS FROM PROJECTS API
+    ////////////////////////////////////
+
+    let projects;
+
+    $.ajax("https://your-projects-api.herokuapp.com/projects")
+        .then(data => {
+            projects = data.map(project => {
+                return {
+                    name: project.name,
+                    img: project.image,
+                    desc: project.description,
+                    live: project.demo, 
+                    github: project.repo,
+                    tech: project.tech
+                }
+            })
+            renderProj(projects)
+        })
+
+    const $logo = $("#logo")
+    const $navIcon = $("#nav-icon")
+    const $menu = $("#menu")
+    let menuOpen = false, $scrollPos = $(window).scrollTop()
+
+    /////////////////////////////////////
+    /// MENU FUNCTIONALITY 
+    ////////////////////////////////////
+    
+    const menuFunctionality = () => {
+        if (menuOpen) {
+            $menu.removeClass("open")
+            $logo.removeClass("black-text")
+            $navIcon.removeClass("open-color")
+            menuOpen = false
+        } else {
+            $menu.addClass("open")
+            $logo.addClass("black-text")
+            $navIcon.addClass("open-color")
+            menuOpen = true
+        }
+    }
+    
+    $navIcon.click(e => {
+        menuFunctionality()
+    })
+    
+    $("#menu").children().click(e => {
+        menuFunctionality()
+    })
+    
+    /////////////////////////////////////
+    /// RENDER THE PROJECTS
+    ////////////////////////////////////
+
+    const $projContainer = $(".projects-container")
+    let $projects
+        
+    const renderProj  = projects => {
+        projects.forEach((project, index) => {
+            const $li = $("<li>").addClass("cube")
+            const $face1 = $("<div>").addClass(`face ${project.tech}`)
+            const $face2 = $("<div>").addClass("face project")
+            const $content = $("<div>").addClass("project-content")
+            $content.html(
+                `
+                    <div class="project-content-top">
+                        <p>${project.name}</p>
+                    </div>
+                `
+            )
+            const $button = $("<div>").addClass("project-content-bottom button").text("Learn More")
+            $button.click(e => {
+                renderModal(project)
+            })
+            $content.append($button)
+            $face2.append($content)
+            $face1.css("background-image", `url(${project.img})`)
+            $li.append($face1)
+            $li.append($face2)
+            $projContainer.append($li)
+        })
+        $projects = $projContainer.children()
+    }
+        
+    const renderModal = project => {
+        const $modal = $("<div>").addClass("modal")
+        const $modalContent = $("<div>").addClass("modal-container").css("background", `url(${project.img})`)
+        $modalContent.html(
+            `
+                <div class="modal-content">
+                    <div>
+                        <p class="modal-header">${project.name}</p>
+                        <p class="modal-desc">${project.desc}</p>
+                    </div>
+                    <div class="modal-links">
+                        <a class="button" href="${project.live}" target="_blank">Live Demo</a>
+                        <a class="button" href="${project.github}" target="_blank">View Code</a>
+                    </div>
+                </div>
+            `
+        )
+        $modalContent.click(e => e.stopPropagation())
+        $modal.click(e => $modal.remove())
+        $modal.append($modalContent)
+        $("body").append($modal)
+    }
+
+    //////////////////////
+    /// SKILLS ANIMATIONS
+    //////////////////////
+
     const speed  = {
         animation: 9000,
         delay: 1100
     }
+
     const $frontEnd = $("#frontend li"), $backEnd = $("#backend li")
     let frontEnd, backEnd;
 
@@ -114,7 +175,33 @@ $(() => {
     applyAnimations(frontEnd, 'left')
     applyAnimations(backEnd, 'right')
 
+    ////////////////////
+    /// WINDOW SCROLL 
+    ////////////////////
+
+    const $landingPagePos = window.innerHeight
+    const $secondaryBgPos = $("#secondary-bg").position().top
+    
+    $(window).scroll(() => {
+        $scrollPos = $(window).scrollTop()
+    
+        if ($scrollPos > $landingPagePos) {
+            $("nav").addClass("blur")
+        } else {
+            $("nav").removeClass("blur")
+        }
+    
+        if ($scrollPos + 54 > $secondaryBgPos && !menuOpen) {
+            $logo.addClass("dark-text")
+            $navIcon.addClass("black-text")
+        } else {
+            $logo.removeClass("dark-text")
+            $navIcon.removeClass("black-text")
+        }
+    })
+
     window.addEventListener('resize', async () => {
+        renderProj(projects)
         $("#frontend").empty()
         $("#backend").empty()
         generateSpeed()
@@ -122,120 +209,4 @@ $(() => {
         $("#frontend").append($frontEnd)
         $("#backend").append($backEnd)
     })
-})
-
-///////////////////////////////
-//// PROJECT RENDERING 
-///////////////////////////////
-
-const $projContainer = $(".projects-container")
-let $projects
-
-const renderProj  = projects => {
-    let i = 0
-    for (let project of projects) {
-        const $li = $("<li>").addClass("cube left-end x")
-        const $face1 = $("<div>").addClass(`face ${project.tech}`)
-        const $face2 = $("<div>").addClass("face project")
-        const $content = $("<div>").addClass("project-content")
-        $content.html(
-            `
-                <div class="project-content-top">
-                    <p>${project.name}</p>
-                </div>
-            `
-        )
-        const $button = $("<div>").addClass("project-content-bottom button").text("Learn More")
-        $button.click(e => {
-            renderModal(projects[i])
-        })
-        $content.append($button)
-        $face2.append($content)
-        $face1.css("background-image", `url(${project.img})`)
-        $li.append($face1)
-        $li.append($face2)
-        $projContainer.append($li)
-        i++
-    }
-    $projects = $projContainer.children()
-}
-
-const renderModal = project => {
-    const $modal = $("<div>").addClass("modal")
-    const $modalContent = $("<div>").addClass("modal-container").css("background", `url(${project.img})`)
-    $modalContent.html(
-        `
-            <div class="modal-content">
-                <div>
-                    <p class="modal-header">${project.name}</p>
-                    <p class="modal-desc">${project.desc}</p>
-                </div>
-                <div class="modal-links">
-                    <a class="button" href="${project.live}" target="_blank">Live Demo</a>
-                    <a class="button" href="${project.github}" target="_blank">View Code</a>
-                </div>
-            </div>
-        `
-    )
-    $modalContent.click(e => e.stopPropagation())
-    $modal.click(e => $modal.remove())
-    $modal.append($modalContent)
-    $("body").append($modal)
-}
-
-
-///////////////////////////////////////
-/// PROJECT NAV FUNCTIONALITY ANIMATION
-///////////////////////////////////////
-
-const $projNav = $(".proj-nav").children()
-const $underline = $("hr")
-
-for (let i = 0; i < 4; i++) {
-    $projNav.eq(i).click(e => {
-        updateProjNav(e.target.id)
-        filterProjects(e.target.id)
-    })
-}
-
-const updateProjNav = id => {
-    $underline.removeAttr('id')
-    $underline.attr('id', `_${id}`)
-}
-
-const filterProjects = category => {
-    console.log(category)
-    for (let i = 0; i < $projects.length; i++) {
-        const $project = $projects.eq(i)
-        if ($project.hasClass(category) || category === "all") {
-            $project.removeClass("gone")
-        } else {
-            $project.addClass("gone")
-        }
-    }
-}
-
-////////////////////////
-/// SCROLLING 
-////////////////////////
-
-const $landingPagePos = window.innerHeight
-const $secondaryBgPos = $("#secondary-bg").position().top
-
-$(window).scroll(() => {
-    $scrollPos = $(window).scrollTop()
-
-    if ($scrollPos > $landingPagePos) {
-        $("nav").addClass("blur")
-    } else {
-        $("nav").removeClass("blur")
-    }
-
-    if ($scrollPos + 54 > $secondaryBgPos && !menuOpen) {
-        $logo.addClass("dark-text")
-        $navIcon.addClass("black-text")
-    } else {
-        $logo.removeClass("dark-text")
-        $navIcon.removeClass("black-text")
-    }
 })
